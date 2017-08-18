328c53a68ed162efdb03f4bcf8fa19a8391c9c39
juan_sca <juansca1229@gmail.com>
2017-08-18 12:16:14 -0300

    Extract data from many files

--- image_classif/data_extractor.py
+++ image_classif/data_extractor.py
@@ -15,29 +15,33 @@
 #  }
 # cont.insertBefore(divv,cont.childNodes[0]);
 import urllib.request
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 
 
-def download_image(url, name):
+def download_image(dclass, url, name):
     """Download a image from a url."""
-    name = 'images_train/' + name + '.jpg'
+    name = 'data/' + dclass + '/' + name + '.jpg'
     urllib.request.urlretrieve(url, name)
 
 
 if __name__ == '__main__':
 
-    with open('data.txt', 'r') as data:
-        i = 0
-        errors = []
-        for url in data:
-            i += 1
-            if url[:4] == 'http':
-                try:
-                    download_image(url, str(i))
-                except HTTPError:
-                    print("error in", url)
-                    errors.append(url)
-            if i % 20 == 0:
-                print(i, "images downloaded")
-        print("The number of erros in downloading all file is", len(errors))
-        print(errors)
+    data_classes = ['data_flood', 'data_river']
+    for dclass in data_classes:
+        print("\n\nDownloading files from", dclass)
+        with open(dclass + '.txt', 'r') as data:
+            i = 0
+            errors = []
+            for url in data:
+                i += 1
+                if url[:4] == 'http':
+                    try:
+                        download_image(dclass, url, str(i))
+                    except (HTTPError, URLError):
+                        print("error in", url)
+                        errors.append(url)
+                if i % 20 == 0:
+                    print(i, "images downloaded")
+            print("The number of erros in downloading all file in", dclass,
+                  "is", len(errors))
+            print(errors)
