9b1ffe86780aa356c7a8ebfdcaf1b1fff251f668
Juan Scavuzzo <juansca1229@gmail.com>
2017-09-18 11:35:46 -0300

    Delete config.py

--- post_extraction/config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'DaHWzPgCItGbHTHcpMtcUGjjp'
-consumer_secret = 'c1glG2gDSCuSdHqXeO00OUWX9X7yhsEI7at7Ndhu5ooGMlO8Gn'
-access_token = '816430009-3047065013-ACEoUsLMHv7ngsGzyyoGiUy9HGs9tymlM0ZaVyp'
-access_secret = '6HKp9z1izSFd9inuQLbs5ojIN0APRryAPoWVJCFHF3R8Z'
